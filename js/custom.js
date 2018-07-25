ef84ab322565809b93e83031e170921aaf8b6541
ronnelsz <41312917+ronnelsz@users.noreply.github.com>
2018-07-25 09:14:27 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+alert('it works'); 
+
+smoothScroll: function() {
+        // Smooth Scrolling
+        $('a[href*=#]:not([href=#carousel-example-generic])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 500);
+                    return false;
+                }
+            }
+        });
+    },
+        
+        
+ $(document).ready(function() {
+
+      appMaster.smoothScroll();
+     
+ });
